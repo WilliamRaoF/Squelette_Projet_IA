9c8498712802cff19ed579d523528b4d1fe6319f
et3rn <et3rn@ARACNO>
2025-02-15 18:24:57 +0100

    dernier push add PROTECT de fsm

--- Project1/Enemy.cpp
+++ Project1/Enemy.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 Enemy::Enemy(float x, float y) : Entity(x, y, sf::Color::Red) {}
-
+enum Enemy::State { PATROL, CHASE, SEARCH,PROTECT };
 void Enemy::setPath(std::vector<sf::Vector2i> newPath)
 {
 	if (!newPath.empty()) {
@@ -12,24 +12,29 @@ void Enemy::setPath(std::vector<sf::Vector2i> newPath)
 }
 
 void Enemy::update(float deltaTime, Grid& grid) {
-	if (currentIndexPath >= path.size())
-		return;
+    if (currentIndexPath >= path.size())
+        return;
+
     switch (currentState) {
     case PATROL:
-       //fonction de patrouille
-        //if (/*fonction de détection du player*/) currentState = CHASE;
-        //break;
+        //fonction de patrouille
+         //if (/*fonction de détection du player*/) currentState = CHASE;
+         //break;
 
     case CHASE:
-       //fonction de chase avec le pathfinding
-        //if (!/*fonction de détection du player*/) {
-        //    currentState = SEARCH;
-        //}
+        //fonction de chase avec le pathfinding
+         //if (!/*fonction de détection du player*/) {
+         //    currentState = SEARCH;
+         //}
         break;
 
     case SEARCH:
         break;
+    
+    case PROTECT: 
+        break;
+    
+    default: 
+        break;
     }
-
-
 }
--- Project1/Enemy.hpp
+++ Project1/Enemy.hpp
@@ -2,19 +2,21 @@
 #define ENEMY_HPP
 
 #include "Entity.hpp"
-
+#include <iostream>
 class Enemy : public Entity {
 public:
+
+    enum State;
     static constexpr float SPEED = 100.0f;
     Enemy(float x, float y);
 
     void setPath(std::vector<sf::Vector2i> newPath);
 
     void update(float deltaTime, Grid& grid) override;
-    enum State { PATROL, CHASE, SEARCH };
-    State currentState;
+   
+   
 private:
-
+    State currentState;
     sf::Vector2i gridPosition;
     std::vector<sf::Vector2i> path;
     int currentIndexPath = 0;
